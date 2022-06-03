972a71f96c5a1c9a0d6dc6d67ab49a40900f31d9
Emmanuel Girard <e.girard@f5.com>
2022-06-03 09:47:01 -0400

    Add Comments on ExecutionPolicy

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -1,3 +1,14 @@
+Write-Host "You might need to run as Administrator and loose the ExecutionPolicy option when running the script." -ForegroundColor Yellow
+Write-Host ""
+Write-Host "Get the current ExcutionPolicy value in case you want to put it back after the installation."
+Write-Host "Get-ExecutionPolicy" -ForegroundColor Green
+Get-ExecutionPolicy
+Write-Host ""
+Write-Host "Only this session will be affected."
+Write-Host "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process" -ForegroundColor Green
+Write-Host ""
+Write-Host "CurrentUser Profile"
+Write-Host "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser" -ForegroundColor Green
 
 #By Using PowerShell Core we can use the same code accross all the platforms.
 if ($PSVersionTable.PSVersion.Major -lt 6) {
@@ -11,7 +22,9 @@ try {
     Install-Module ImportExcel -Scope CurrentUser
     Install-Module Microsoft.Graph -Scope CurrentUser
 }
-catch { 
+catch {
+    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+
     if ($isWindows) {
         Write-Host "Specific to Windows:" -ForegroundColor Red
         Write-Host "run this command to delete the PowerShell LockDown Key"
